dc7c357ebe4d6a2acea75189c1312d519008d537
Constantin CHTANKO <constantin.chtanko@apside.com>
2023-11-20 17:46:26 +0100

    Tests

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,9 +1,4 @@
-import 'jest-preset-angular/setup-jest';
-import { TestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/testing';
+import { TestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
 
-beforeAll( ()=> {
-    TestBed.initTestEnvironment(BrowserDynamicTestingModule, 
-        platformBrowserDynamicTesting());
-});
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
